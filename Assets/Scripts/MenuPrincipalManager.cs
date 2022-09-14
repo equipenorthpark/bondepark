85405f07da519f0550ed3871a44a7b67d43167b3
POLLYANNA MELQUIADES DA SILVA <pollyanna.melquiades@alunoescolassociais.org.br>
2022-09-14 15:27:51 -0300

    Foi criado e programado um Menu Principal

---
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDoJogo;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+    
+}
