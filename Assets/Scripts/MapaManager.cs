ac8041eb9433a787fe524f80a6fe15d6a0ea8dd3
POLLYANNA MELQUIADES DA SILVA <pollyanna.melquiades@alunoescolassociais.org.br>
2022-09-22 17:24:38 -0300

    Foi criado e pré-programado um Mapa de Navegação

---
+++ Assets/Scripts/MapaManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapaManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLugarDoJogoI;
+    [SerializeField] private string nomeDoLugarDoJogoII;
+
+    public void CasaNick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Praça()
+    {
+        SceneManager.LoadScene("Praça");
+    }
+
+    public void Lago()
+    {
+
+    }
+}
